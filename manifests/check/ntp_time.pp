ae96facabafe4c8de482d6fcb704726f0adeb748
Matthias Saou <matthias@saou.eu>
2014-09-16 10:59:06 +0200

    Cosmetic updates to ntp_time and update ChangeLog

--- manifests/check/ntp_time.pp
+++ manifests/check/ntp_time.pp
@@ -1,43 +1,47 @@
-define nagios::check::ntp_time ( $args = '' ) {
+define nagios::check::ntp_time (
+  $args       = '',
+  $ntp_server = $::nagios_check_ntp_time_target,
+) {
 
-    # Generic overrides
-    if $::nagios_check_ntp_time_ensure != 'absent' {
-      Package <| tag == 'nagios-plugins-ntp' |>
-    }
+  # Required plugin
+  if $::nagios_check_ntp_time_ensure != 'absent' {
+    Package <| tag == 'nagios-plugins-ntp' |>
+  }
 
-    if $::nagios_check_ntp_time_check_period != '' {
-        Nagios_service { check_period => $::nagios_check_ntp_time_check_period }
-    }
-    if $::nagios_check_ntp_time_notification_period != '' {
-        Nagios_service { notification_period => $::nagios_check_ntp_time_notification_period }
-    }
+  # Generic overrides
+  if $::nagios_check_ntp_time_check_period != '' {
+    Nagios_service { check_period => $::nagios_check_ntp_time_check_period }
+  }
+  if $::nagios_check_ntp_time_notification_period != '' {
+    Nagios_service { notification_period => $::nagios_check_ntp_time_notification_period }
+  }
 
-    # Service specific overrides
-    if $::nagios_check_ntp_time_warning != '' {
-        $warning = $::nagios_check_ntp_time_warning
-    } else {
-        $warning = '1'
-    }
-    if $::nagios_check_ntp_time_critical != '' {
-        $critical = $::nagios_check_ntp_time_critical
-    } else {
-        $critical = '2'
-    }
-    if $::nagios_check_ntp_time_target != '' {
-        $target = $::nagios_check_ntp_time_target
-    } else {
-        $target = '1.rhel.pool.ntp.org'
-    }
+  # Service specific overrides
+  if $::nagios_check_ntp_time_warning != '' {
+    $warning = $::nagios_check_ntp_time_warning
+  } else {
+    $warning = '1'
+  }
+  if $::nagios_check_ntp_time_critical != '' {
+    $critical = $::nagios_check_ntp_time_critical
+  } else {
+    $critical = '2'
+  }
+  if $ntp_server != '' {
+    $target = $ntp_server
+  } else {
+    $target = '1.rhel.pool.ntp.org'
+  }
 
-    nagios::client::nrpe_file { 'check_ntp_time':
-        args => "-H ${target} -w ${warning} -c ${critical} ${args}",
-    }
+  nagios::client::nrpe_file { 'check_ntp_time':
+    args => "-H ${target} -w ${warning} -c ${critical} ${args}",
+  }
 
-    nagios::service { "check_ntp_time_${title}":
-        check_command       => 'check_nrpe_ntp_time',
-        service_description => 'ntp_time',
-        #servicegroups       => 'ntp_time',
-    }
+  nagios::service { "check_ntp_time_${title}":
+    check_command       => 'check_nrpe_ntp_time',
+    service_description => 'ntp_time',
+    #servicegroups       => 'ntp_time',
+  }
 
 }
 
