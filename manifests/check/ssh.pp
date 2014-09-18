cbb413f2bb3e84f610ff2274da580c14cff8755b
Alex Harvey <alex.harvey@team.telstra.com>
2014-09-18 10:19:36 +1000

    Add an sshd check

---
+++ manifests/check/ssh.pp
@@ -0,0 +1,27 @@
+class nagios::check::ssh (
+  $ensure                   = undef,
+  $args                     = $::nagios::client::args,
+  $check_title              = $::nagios::client::host_name,
+  $servicegroups            = undef,
+  $check_period             = $::nagios::client::service_check_period,
+  $contact_groups           = $::nagios::client::service_contact_groups,
+  $first_notification_delay = $::nagios::client::first_notification_delay,
+  $max_check_attempts       = $::nagios::client::service_max_check_attempts,
+  $notification_period      = $::nagios::client::service_notification_period,
+  $use                      = $::nagios::client::service_use,
+) {
+
+  nagios::service { "check_sshd_${check_title}":
+    ensure                   => $ensure,
+    check_command            => "check_ssh!${args}",
+    service_description      => 'sshd',
+    servicegroups            => $servicegroups,
+    check_period             => $check_period,
+    contact_groups           => $contact_groups,
+    first_notification_delay => $first_notification_delay,
+    notification_period      => $notification_period,
+    max_check_attempts       => $max_check_attempts,
+    use                      => $use,
+  }
+
+}
