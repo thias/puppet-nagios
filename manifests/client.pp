b824effc784f40c7ddc6ef62e4bc2698943d1ed6
Matthias Saou <matthias@exoclick.com>
2015-04-21 18:27:32 +0200

    Cosmetic cleanups

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,44 +3,41 @@
 # This is the main class to be included on all nodes to be monitored by nagios.
 #
 class nagios::client (
-    $nagios_host_name            = $::nagios_host_name,
-    $nagios_server               = $::nagios_server,
-    # nrpe.cfg
-    $nrpe_log_facility           = 'daemon',
-    $nrpe_pid_file               = $nagios::params::nrpe_pid_file,
-    $nrpe_server_port            = '5666',
-    $nrpe_server_address         = undef,
-    $nrpe_user                   = $nagios::params::nrpe_user,
-    $nrpe_group                  = $nagios::params::nrpe_group,
-    $nrpe_allowed_hosts          = '127.0.0.1',
-    $nrpe_dont_blame_nrpe        = '0',
-    $nrpe_command_prefix         = undef,
-    $nrpe_command_timeout        = '60',
-    $nrpe_connection_timeout     = '300',
-    # host defaults
-    $host_address                = $::nagios_host_address,
-    $host_alias                  = $::nagios_host_alias,
-    $host_check_period           = $::nagios_host_check_period,
-    $host_check_command          = $::nagios_host_check_command,
-    $host_contact_groups         = $::nagios_host_contact_groups,
-    $host_hostgroups             = $::nagios_host_hostgroups,
-    $host_notes                  = $::nagios_host_notes,
-    $host_notes_url              = $::nagios_host_notes_url,
-    $host_notification_period    = $::nagios_host_notification_period,
-    $host_use                    = $::nagios_host_use,
-    # service defaults (hint: use host_* or override only service_use for efficiency)
-    $service_check_period        = $::nagios_service_check_period,
-    $service_contact_groups      = $::nagios_service_contact_groups,
-    $service_max_check_attempts  = $::nagios_service_max_check_attempts,
-    $service_notification_period = $::nagios_service_notification_period,
-    $service_use                 = 'generic-service',
-    # other
-    $plugin_dir                  = $nagios::params::plugin_dir,
-    $selinux                     = true
+  $nagios_host_name            = $::nagios_host_name,
+  $nagios_server               = $::nagios_server,
+  # nrpe.cfg
+  $nrpe_log_facility           = 'daemon',
+  $nrpe_pid_file               = $nagios::params::nrpe_pid_file,
+  $nrpe_server_port            = '5666',
+  $nrpe_server_address         = undef,
+  $nrpe_user                   = $nagios::params::nrpe_user,
+  $nrpe_group                  = $nagios::params::nrpe_group,
+  $nrpe_allowed_hosts          = '127.0.0.1',
+  $nrpe_dont_blame_nrpe        = '0',
+  $nrpe_command_prefix         = undef,
+  $nrpe_command_timeout        = '60',
+  $nrpe_connection_timeout     = '300',
+  # host defaults
+  $host_address                = $::nagios_host_address,
+  $host_alias                  = $::nagios_host_alias,
+  $host_check_period           = $::nagios_host_check_period,
+  $host_check_command          = $::nagios_host_check_command,
+  $host_contact_groups         = $::nagios_host_contact_groups,
+  $host_hostgroups             = $::nagios_host_hostgroups,
+  $host_notes                  = $::nagios_host_notes,
+  $host_notes_url              = $::nagios_host_notes_url,
+  $host_notification_period    = $::nagios_host_notification_period,
+  $host_use                    = $::nagios_host_use,
+  # service defaults (hint: use host_* or override only service_use for efficiency)
+  $service_check_period        = $::nagios_service_check_period,
+  $service_contact_groups      = $::nagios_service_contact_groups,
+  $service_max_check_attempts  = $::nagios_service_max_check_attempts,
+  $service_notification_period = $::nagios_service_notification_period,
+  $service_use                 = 'generic-service',
+  # other
+  $plugin_dir                  = $nagios::params::plugin_dir,
+  $selinux                     = true
 ) inherits ::nagios::params {
-
-  # We are checking facts, which are strings (not booleans!)
-  # lint:ignore:quoted_booleans
 
   # Set the variables to be used, including scoped from elsewhere, based on
   # the optional fact or parameter from here
@@ -61,91 +58,89 @@ class nagios::client (
     alias  => $nagios::params::nrpe_package_alias,
   }
 
-    # Most plugins use nrpe, so we install it everywhere
-    service { $nagios::params::nrpe_service:
-        ensure    => running,
-        enable    => true,
-        hasstatus => true,
-        subscribe => File[$nagios::params::nrpe_cfg_file],
-    }
-    file { $nagios::params::nrpe_cfg_file:
-        owner   => 'root',
-        group   => $nrpe_group,
-        mode    => '0640',
-        content => template('nagios/nrpe.cfg.erb'),
-        require => Package['nrpe']
-    }
-    # Included in the package, but we need to enable purging
-    file { $nagios::params::nrpe_cfg_dir:
-        ensure  => directory,
-        owner   => 'root',
-        group   => $nrpe_group,
-        mode    => '0750',
-        purge   => true,
-        recurse => true,
-        require => Package['nrpe'],
-    }
-    # Create resource for the check_* parent resource
-    file { $nagios::client::plugin_dir:
-        ensure  => directory,
-        require => Package['nrpe'],
-    }
+  # Most plugins use nrpe, so we install it everywhere
+  service { $nagios::params::nrpe_service:
+    ensure    => 'running',
+    enable    => true,
+    hasstatus => true,
+    subscribe => File[$nagios::params::nrpe_cfg_file],
+  }
+  file { $nagios::params::nrpe_cfg_file:
+    owner   => 'root',
+    group   => $nrpe_group,
+    mode    => '0640',
+    content => template('nagios/nrpe.cfg.erb'),
+    require => Package['nrpe']
+  }
+  # Included in the package, but we need to enable purging
+  file { $nagios::params::nrpe_cfg_dir:
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => $nrpe_group,
+    mode    => '0750',
+    purge   => true,
+    recurse => true,
+    require => Package['nrpe'],
+  }
+  # Create resource for the check_* parent resource
+  file { $nagios::client::plugin_dir:
+    ensure  => 'directory',
+    require => Package['nrpe'],
+  }
 
-    # Where to store configuration for our custom nagios_* facts
-    file { '/etc/nagios/facter':
-        ensure  => directory,
-        purge   => true,
-        recurse => true,
-        require => Package['nrpe'],
-    }
+  # Where to store configuration for our custom nagios_* facts
+  file { '/etc/nagios/facter':
+    ensure  => 'directory',
+    purge   => true,
+    recurse => true,
+    require => Package['nrpe'],
+  }
 
-    # The initial fact, to be used to know if a node is a nagios client
-    nagios::client::config { 'client': value => 'true' }
+  # The initial fact, to be used to know if a node is a nagios client
+  nagios::client::config { 'client': value => 'true' }
 
-    # The main nagios_host entry
-    nagios::host { $host_name:
-        server              => $server,
-        address             => $host_address,
-        host_alias          => $host_alias,
-        check_period        => $host_check_period,
-        check_command       => $host_check_command,
-        contact_groups      => $host_contact_groups,
-        hostgroups          => $host_hostgroups,
-        notes               => $host_notes,
-        notes_url           => $host_notes_url,
-        notification_period => $host_notification_period,
-        use                 => $host_use,
-    }
+  # The main nagios_host entry
+  nagios::host { $host_name:
+    server              => $server,
+    address             => $host_address,
+    host_alias          => $host_alias,
+    check_period        => $host_check_period,
+    check_command       => $host_check_command,
+    contact_groups      => $host_contact_groups,
+    hostgroups          => $host_hostgroups,
+    notes               => $host_notes,
+    notes_url           => $host_notes_url,
+    notification_period => $host_notification_period,
+    use                 => $host_use,
+  }
 
-    # TODO: Remove once all check/*.pp files are updated
-    Nagios_service {
-        use => 'generic-service,nagiosgraph-service',
-    }
+  # TODO: Remove once all check/*.pp files are updated
+  Nagios_service {
+    use => 'generic-service,nagiosgraph-service',
+  }
 
-    # Enable all default checks by... default
-    # Old style with facts overrides
-    class { '::nagios::defaultchecks': }
-    # New style with hiera overrides
-    class { '::nagios::check::conntrack': }
-    class { '::nagios::check::cpu': }
-    class { '::nagios::check::load': }
-    class { '::nagios::check::ntp_time': }
-    class { '::nagios::check::ram': }
-    if $::nagios_mysqld {
-      class { '::nagios::check::mysql_health': }
-    }
-    if $::nagios_memcached {
-      class { '::nagios::check::memcached': }
-    }
+  # Enable all default checks by... default
+  # Old style with facts overrides
+  class { '::nagios::defaultchecks': }
+  # New style with hiera overrides
+  class { '::nagios::check::conntrack': }
+  class { '::nagios::check::cpu': }
+  class { '::nagios::check::load': }
+  class { '::nagios::check::ntp_time': }
+  class { '::nagios::check::ram': }
+  if $::nagios_mysqld {
+    class { '::nagios::check::mysql_health': }
+  }
+  if $::nagios_memcached {
+    class { '::nagios::check::memcached': }
+  }
 
-    # With selinux, some nrpe plugins require additional rules to work
-    if $selinux and $::selinux_enforced {
-        selinux::audit2allow { 'nrpe':
-            source => "puppet:///modules/${module_name}/messages.nrpe",
-        }
+  # With selinux, some nrpe plugins require additional rules to work
+  if $selinux and $::selinux_enforced {
+    selinux::audit2allow { 'nrpe':
+      source => "puppet:///modules/${module_name}/messages.nrpe",
     }
-
-  # lint:endignore
+  }
 
 }
 
--- manifests/service.pp
+++ manifests/service.pp
@@ -19,44 +19,44 @@ define nagios::service (
   $use                      = $nagios::client::service_use,
 ) {
 
-    # Work around being passed undefined variables resulting in ''
-    $final_check_period = $check_period ? {
-        ''      => $nagios::client::service_check_period,
-        undef   => $nagios::client::service_check_period,
-        default => $check_period,
-    }
-    $final_max_check_attempts = $max_check_attempts ? {
-        ''      => $nagios::client::service_max_check_attempts,
-        undef   => $nagios::client::service_max_check_attempts,
-        default => $max_check_attempts,
-    }
-    $final_notification_period = $notification_period ? {
-        ''      => $nagios::client::service_notification_period,
-        undef   => $nagios::client::service_notification_period,
-        default => $notification_period,
-    }
-    $final_use = $use ? {
-        ''      => $nagios::client::service_use,
-        undef   => $nagios::client::service_use,
-        default => $use,
-    }
+  # Work around being passed undefined variables resulting in ''
+  $final_check_period = $check_period ? {
+    ''      => $nagios::client::service_check_period,
+    undef   => $nagios::client::service_check_period,
+    default => $check_period,
+  }
+  $final_max_check_attempts = $max_check_attempts ? {
+    ''      => $nagios::client::service_max_check_attempts,
+    undef   => $nagios::client::service_max_check_attempts,
+    default => $max_check_attempts,
+  }
+  $final_notification_period = $notification_period ? {
+    ''      => $nagios::client::service_notification_period,
+    undef   => $nagios::client::service_notification_period,
+    default => $notification_period,
+  }
+  $final_use = $use ? {
+    ''      => $nagios::client::service_use,
+    undef   => $nagios::client::service_use,
+    default => $use,
+  }
 
-    # Support an array of tags for multiple nagios servers
-    $service_tag = regsubst($server,'^(.+)$','nagios-\1')
-    @@nagios_service { $title:
-        ensure                   => $ensure,
-        host_name                => $host_name,
-        check_command            => $check_command,
-        service_description      => $service_description,
-        servicegroups            => $servicegroups,
-        check_period             => $final_check_period,
-        contact_groups           => $contact_groups,
-        first_notification_delay => $first_notification_delay,
-        max_check_attempts       => $final_max_check_attempts,
-        notification_period      => $final_notification_period,
-        use                      => $final_use,
-        tag                      => $service_tag,
-    }
+  # Support an array of tags for multiple nagios servers
+  $service_tag = regsubst($server,'^(.+)$','nagios-\1')
+  @@nagios_service { $title:
+    ensure                   => $ensure,
+    host_name                => $host_name,
+    check_command            => $check_command,
+    service_description      => $service_description,
+    servicegroups            => $servicegroups,
+    check_period             => $final_check_period,
+    contact_groups           => $contact_groups,
+    first_notification_delay => $first_notification_delay,
+    max_check_attempts       => $final_max_check_attempts,
+    notification_period      => $final_notification_period,
+    use                      => $final_use,
+    tag                      => $service_tag,
+  }
 
 }
 
