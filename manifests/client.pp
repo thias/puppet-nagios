58d567a8e0007ef35ca24d42a03d781c51630c81
Mark Hamzy <hamzy@us.ibm.com>
2015-08-25 09:49:29 -0500

    nagios-plugins-mysql_health does not exist on Trusty

--- manifests/client.pp
+++ manifests/client.pp
@@ -131,7 +131,18 @@ class nagios::client (
   class { '::nagios::check::ping6': }
   class { '::nagios::check::ram': }
   if $::nagios_mysqld {
-    class { '::nagios::check::mysql_health': }
+    case $::operatingsystem {
+      'RedHat', 'Fedora', 'CentOS', 'Scientific', 'Amazon': {
+        class { '::nagios::check::mysql_health': }
+      }
+      'Debian', 'Ubuntu': {
+        # nagios-plugins-mysql_health doesn't exist for Trusty
+        # https://launchpad.net/ubuntu/trusty/+search?text=nagios-plugins
+      }
+      default: {
+        class { '::nagios::check::mysql_health': }
+      }
+    }
   }
   if $::nagios_memcached {
     class { '::nagios::check::memcached': }
