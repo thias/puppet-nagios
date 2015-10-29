920abf6b47086e284f696f94786efb8ca82a12eb
Jesus Rodrigo <jesus@exads.com>
2015-10-29 18:03:09 +0100

    Ruby 1.8.x doesn't like .method calls

--- lib/facter/nagios_ipaddress6.rb
+++ lib/facter/nagios_ipaddress6.rb
@@ -12,11 +12,11 @@ if Facter.version.to_f < 3.0
 
   def get_address_after_token(output, token)
 
-    String(output).scan(/#{token}\s?((?>[0-9,a-f,A-F]*\:{1,2})+[0-9,a-f,A-F]{0,4})/)
-      .select { |match| valid_addr?(match.first) }
-      .flatten
-      .sort_by { |x| x.length }
-      .shift
+    String(output).scan(/#{token}\s?((?>[0-9,a-f,A-F]*\:{1,2})+[0-9,a-f,A-F]{0,4})/).
+      select { |match| valid_addr?(match.first) }.
+      flatten.
+      sort_by { |x| x.length }.
+      shift
   end
 
   Facter.add(:nagios_ipaddress6) do
@@ -29,14 +29,14 @@ if Facter.version.to_f < 3.0
 else
   Facter.add(:nagios_ipaddress6) do
     setcode do
-      Facter.value(:networking)['interfaces']
-        .values
-        .map { |x| x['bindings6'] }
-        .flatten
-        .map { |x| x['address'] }
-        .select { |x| valid_addr? x }
-        .sort_by { |x| x.length }
-        .shift
+      Facter.value(:networking)['interfaces'].
+        values.
+        map { |x| x['bindings6'] }.
+        flatten.
+        map { |x| x['address'] }.
+        select { |x| valid_addr? x }.
+        sort_by { |x| x.length }.
+        shift
     end
   end
 end
