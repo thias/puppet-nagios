8509032ab83173ab1cc8e0240623ed9e5b12ebbb
Michał Lisowski <michal@exads.com>
2016-11-23 14:53:20 +0000

    Add check_zookeeper for Zookeeper monitoring

---
+++ manifests/check/zookeeper.pp
@@ -0,0 +1,55 @@
+class nagios::check::zookeeper (
+  $args                               = undef,
+  $check_period                       = undef,
+  $first_notification_delay           = undef,
+  $notification_period                = undef,
+  $keys_enabled                       = [],
+  $keys_disabled                      = [],
+  $pkg                                = true,
+  $ensure                             = undef,
+  $leader                             = false,
+  $plugin                             = 'check_zookeeper',
+  # Keys
+  $args_zk_avg_latency                = '-w 1 -c 10',
+  $args_zk_max_latency                = '-w 10 -c 20',
+  $args_zk_outstanding_requests       = '-w 10 -c 20',
+  $args_zk_open_file_descriptor_count = '-w 3481 -c 3890',
+  # Leader only keys
+  $args_zk_pending_syncs              = '-w 10 -c 20',
+  $args_zk_followers                  = '-w 3 -c 3',
+) {
+
+  # Generic overrides
+  if $check_period {
+    Nagios_service { check_period => $::nagios_check_zookeeper_period }
+  }
+  if $first_notification_delay {
+    Nagios_service { first_notification_delay => $::nagios_check_zookeeper_first_notification_delay }
+  }
+  if $notification_period {
+    Nagios_service { notification_period => $::nagios_check_zookeeper_notification_period }
+  }
+
+  # Optional package containing the script
+  if $pkg {
+    $pkgname = 'nagios-plugins-zookeeper'
+    $pkgensure = $ensure ? {
+      'absent' => 'absent',
+      default  => 'installed',
+    }
+    package { $pkgname: ensure => $pkgensure }
+  }
+
+  # Modes-specific definition
+  nagios::check::zookeeper::key { [
+    'zk_avg_latency',
+    'zk_max_latency',
+    'zk_outstanding_requests',
+    'zk_open_file_descriptor_count',
+    'zk_pending_syncs',
+    'zk_followers',
+  ]:
+  }
+
+}
+
---
+++ manifests/check/zookeeper/key.pp
@@ -0,0 +1,56 @@
+# Define:
+#
+# Only meant to be called from check::zookeeper, and looking up
+# many variables directly there.
+#
+
+define nagios::check::zookeeper::key () {
+
+  $key = $title
+
+  # Get the variables we need
+  $check_title   = $::nagios::client::host_name
+  $args          = $::nagios::check::zookeeper::args
+  $keys_enabled  = $::nagios::check::zookeeper::keys_enabled
+  $keys_disabled = $::nagios::check::zookeeper::keys_disabled
+  $ensure        = $::nagios::check::zookeeper::ensure
+  $leader        = $::nagios::check::zookeeper::leader
+  $plugin        = $::nagios::check::zookeeper::plugin
+
+  # Get the args passed to the main class for our key
+  $args_key = getvar("nagios::check::zookeeper::args_${key}")
+
+  # Disable follower only keys if needed
+  if $leader == false {
+    $keys_disabled_final = concat($keys_disabled, 'zk_pending_syncs', 'zk_followers')
+  } else {
+    $keys_disabled_final = $keys_disabled
+  }
+
+  if ( ( $keys_enabled == [] and $keys_disabled_final == [] ) or
+    ( $keys_enabled != [] and $key in $keys_enabled ) or
+    ( $keys_disabled_final != [] and ! ( $key in $keys_disabled_final ) ) )
+  {
+    nagios::client::nrpe_file { "check_zookeeper_${key}":
+      ensure    => $ensure,
+      plugin    => $plugin,
+      args      => "${args} --output=nagios --key=${title} ${args_key}",
+    }
+    nagios::service { "check_zookeeper_${key}_${check_title}":
+      ensure              => $ensure,
+      check_command       => "check_nrpe_zookeeper_${key}",
+      service_description => "zookeeper_${key}",
+      servicegroups       => 'zookeeper',
+    }
+  } else {
+    nagios::client::nrpe_file { "check_zookeeper_${key}":
+      ensure => 'absent',
+    }
+    nagios::service { "check_zookeeper_${key}_${check_title}":
+      ensure        => 'absent',
+      check_command => 'foo',
+    }
+  }
+
+}
+
