598957475f04bfba6fe5a1bf4386b1a5101bb6fb
David Castañeda <david.castaneda@exoclick.com>
2017-08-28 17:42:25 +0200

    Fix multi master redis check. - Use fqdn fact - Add args param for sentinel multi master

--- manifests/check/redis_sentinel_mmasters.pp
+++ manifests/check/redis_sentinel_mmasters.pp
@@ -1,18 +1,18 @@
 define nagios::check::redis_sentinel_mmasters (
-  $ensure                   = undef,
-  $servicegroups            = 'redis',
+  $ensure        = undef,
+  $fqdn,
   $port,
+  $args          = undef,
 ) {
 
   $master = $title
-  $lan_fqdn = regsubst($::fqdn, '\.[a-z]{2,}$','.lan')
-  $arg_domain = "-H ${lan_fqdn} "
+  $arg_domain = "-H ${fqdn} "
   $arg_port = "-p ${port} "
   $arg_master = "-m ${master} "
 
-  $global_args = strip("!${arg_domain}${arg_master}${arg_port}")
+  $global_args = strip("!${arg_domain}${arg_master}${arg_port}${args}")
 
-  nagios::service { "check_redis_sentinel_${lan_fqdn}_${master}":
+  nagios::service { "check_redis_sentinel_${fqdn}_${master}":
     check_command       => "check_redis_sentinel${global_args}",
     service_description => "redis_sentinel_${master}",
     servicegroups       => 'redis',
