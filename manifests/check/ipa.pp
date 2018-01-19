34b911e619f2e4bf134a55ef76f7166611b20ddf
David Castañeda <david.castaneda@exoclick.com>
2018-01-19 13:41:42 +0100

    ipa check: Use full path for ipactl command

--- manifests/check/ipa.pp
+++ manifests/check/ipa.pp
@@ -1,7 +1,6 @@
 class nagios::check::ipa (
   $ensure                   = undef,
   $args                     = '',
-  $ipactl_bin               = '/sbin/ipactl',
   $check_title              = $::nagios::client::host_name,
   $servicegroups            = undef,
   $check_period             = $::nagios::client::service_check_period,
@@ -15,7 +14,7 @@ class nagios::check::ipa (
   nagios::client::nrpe_plugin { 'check_ipa':
     ensure   => $ensure,
     # The check executes ipactl using sudo
-    sudo_cmd => "${ipactl_bin} status",
+    sudo_cmd => '/usr/sbin/ipactl status',
   }
 
   nagios::client::nrpe_file { 'check_ipa':
