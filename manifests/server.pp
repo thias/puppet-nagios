a5023e6d658ceade603459065722f6dd5293632f
Michał Lisowski <michal@exads.com>
2018-10-18 16:16:25 +0200

    Pushed too much, restore check_nrpe_tls_files command

--- manifests/server.pp
+++ manifests/server.pp
@@ -980,6 +980,9 @@ class nagios::server (
   nagios_command { 'check_nrpe_ssd':
     command_line => "${nrpe} -c check_ssd",
   }
+  nagios_command { 'check_nrpe_tls_files':
+    command_line => "${nrpe} -c check_tls_files",
+  }
   nagios_command { 'check_nrpe_kafka':
     command_line => "${nrpe} -c check_kafka",
   }
