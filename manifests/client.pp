efe5a915512e24c6bb81d2f0772c5a4ec42fda51
Vincent Tamet <vincent.tamet@exoclick.com>
2019-12-17 12:48:01 +0100

    SELinux updates for RH8

--- manifests/client.pp
+++ manifests/client.pp
@@ -206,6 +206,11 @@ class nagios::client (
     selinux::audit2allow { 'nrpe':
       source => "puppet:///modules/${module_name}/messages.nrpe",
     }
+    selboolean { 'nagios_run_sudo':
+      name       => 'nagios_run_sudo',
+      persistent => true,
+      value      => on,
+    }
   }
 
   # Custom (nrpe) services/nrpe files/plugins support
