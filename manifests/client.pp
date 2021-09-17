27162837d31c9ec5bbc39589aee970216004c276
Robert Brooks <robert.brooks@riskiq.net>
2021-09-17 11:11:03 -0700

    typo

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,7 +4,7 @@
 #
 class nagios::client (
   $nagios_host_name                 = getvar('::nagios_host_name'),
-  $host_fqdn                        = %::fqdn,
+  $host_fqdn                        = $::fqdn,
   $nagios_server                    = getvar('::nagios_server'),
   # nrpe
   $nrpe_package                     = $::nagios::params::nrpe_package,
